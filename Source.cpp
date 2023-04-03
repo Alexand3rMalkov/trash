2723b11dae92054f0f9d6935c6cd021cd56dbe49
Alexand3rMalkov <113231750+Alexand3rMalkov@users.noreply.github.com>
2023-04-03 14:25:34 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,156 +1,41 @@
 #include <iostream>
-#include <cmath>
-#include <stdio.h>;
+#include <stdio.h>
+#include <string>
 using namespace std;
 
-class Triangle {
-private:
-	int a, b, c; //поля для длин сторон
+class Student {
 public:
-	//конструктор с заданными длинами сторон
-	Triangle(int a, int b, int c) {
-		this->a = a;
-		this->b = b;
-		this->c = c;
-	}
-	//методы ля вывода длин сторон
-	void printA() {
-		cout << "a = " << a << endl;
-	}
-	void printB() {
-		cout << "b = " << b << endl;
-	}
-	void printC() {
-		cout << "c = " << c << endl;
-	}
+	int course = 1;
+	int age = 18;
+	int year = 2022;
+	string name = "Vadim";
+};
 
-	int perimeter() {
-		return a + b + c;
+class Mathematic: public Student {
+public:
+	double average_value(int math, int phys, int analitic_geom, int high_math) {
+		double av_value = (math + analitic_geom + phys + high_math) / 4;
+		return av_value;
 	}
-	//метод для расчета площади треугольника по формуле Герона
-	double area() {
-		double p = (a + b + c) / 2.0; //
-		return sqrt(p * (p - a) * (p - b) * (p - c)); //формула Герона
-	}
-	//свойства для получения/установки длин сторон треугольника
-	int getA() {
-		return a;
-	}
-	void setA(int value) {
-		a = value;
-	}
-
-	int getB() {
-		return b;
-	}
-	void setB(int value) {
-		b = value;
-	}
-
-	int getC() {
-		return c;
-	}
-	void setC(int value) {
-		c = value;
-	}
-
-	//свойство для проверки существования треугольника
-	bool valid() const {
-		return (a + b > c && a + c > b && b + c > a);
-	}
-
-	// индексатор для обращения к полям по индексу
-	int& operator[](int index) {
-		switch (index) {
-		case 0:
-			return a;
-			break;
-		case 1:
-			return b;
-			break;
-		case 2:
-			return c;
-			break;
-		}
-	}
-
-	// перегрузка операции ++ (префиксная и постфиксная)
-	Triangle& operator++() { // префиксная форма
-		++a;
-		++b;
-		++c;
-		return *this;
-	}
-	Triangle operator++(int) {
-		Triangle temp(*this);
-		++(*this);
-		return temp;
-	}
-
-	// перегрузка операции -- (префиксная и постфиксная)
-	Triangle& operator--() {
-		--a;
-		--b;
-		--c;
-		return *this;
-	}
-	Triangle operator--(int) {
-		Triangle temp(*this);
-		--(*this);
-		return temp;
-	}
-	// перегрузка констант true и false
-	operator bool() const {
-		return valid();
-	}
-
-	// перегрузка операции *
-	Triangle operator*(int scalar) {
-		return Triangle(a * scalar, b * scalar, c * scalar);
+protected:
+	int math = 5;
+	int phys = 4;
+	int analitic_geom = 4;
+	int high_math = 4;
+	double check(int math, int phys, int analitic_geom, int high_math) {
+		if (average_value(math, phys, analitic_geom, high_math) < 3) cout << "Здравствуй небо в облаках, здравствуй юность в сапогах..." << endl;
+		else cout << "Ура, я умный!" << endl;
 	}
 };
-int main() {
-	system("chcp 1251>NULL");
-	int a, b, c;
-	cout << "Введите длины сторон: " << endl;
-	cout << "Сторона a: ";
-	cin >> a;
-	cout << "Сторона b: ";
-	cin >> b;
-	cout << "Сторона c: ";
-	cin >> c;
-	Triangle t(a, b, c); //треугольник с длинами сторон, созданный пользователем
-	//выводим длины на экран
-	t.printA();
-	t.printB();
-	t.printC();
 
-	cout << "Периметр = " << t.perimeter() << endl;
-	cout << "Площадь = " << t.area() << endl;
-
-	//проверяем на существование треугольника
-	if (t) {
-		cout << "Треугольник существует." << endl;
+class Mathematic_prog : protected Mathematic {
+private:
+	int choose_mark(int math, int phys, int analitic_geom, int high_math) {
+		cout << "Введите оценки по предметам: "
 	}
-	else { cout << "Треугольник не существует." << endl; }
+};
 
-	//обращаемся к полям по индексу
-	cout << "t[0] = " << t[0] << endl;
-	cout << "t[1] = " << t[1] << endl;
-	cout << "t[2] = " << t[2] << endl;
-	++t;
-	cout << "Новые значения " << endl;
-	t.printA();
-	t.printB();
-	t.printC();
-	--t;
-	cout << "Новые значения " << endl;
-	t.printA();
-	t.printB();
-	t.printC();
-	Triangle z = t * 2;
-	cout << "Новые значения " << endl;
-	z.printA();
-	z.printB();
-	z.printC();
-}
+ int main() {
+	 system("chcp 1251>NULL");
+
+ }
