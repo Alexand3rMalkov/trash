878d69ee862d6351b581788fa0c5e8f8c0fcdfa3
Alexand3rMalkov <113231750+Alexand3rMalkov@users.noreply.github.com>
2023-04-06 13:17:04 +0300

    Create student.h

---
+++ student.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class Student {
+public:
+    int course = 1;
+    int age = 18;
+    int year = 2022;
+    string name = "Vadim";
+};
+
+class Mathematic : public Student {
+public:
+    int math = 5;
+    int phys = 4;
+    int analitic_geom = 4;
+    int high_math = 4;
+
+    double check() {
+    }
+
+    double average_value() {
+    }
+
+};
+
+class Mathematic_prog : public Mathematic {
+public:
+    int ch_age() {
+    }
+    int choose_mark() {
+    }
+
+protected:
+    double average_value() {
+    }
+};
