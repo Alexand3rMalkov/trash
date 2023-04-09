8ae7c9481acc31c397d9ee596f9498034bef6f4c
Alexand3rMalkov <113231750+Alexand3rMalkov@users.noreply.github.com>
2023-04-09 16:17:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,41 +1,20 @@
 #include <iostream>
-#include <stdio.h>
 #include <string>
+#include "Student.h"
+
 using namespace std;
 
-class Student {
-public:
-	int course = 1;
-	int age = 18;
-	int year = 2022;
-	string name = "Vadim";
-};
+int main() {
+    system("chcp 1251>NULL");
+    Mathematic_prog student;
+    cout << "Студент " << student.name << ", " << student.course << " курс" << endl;
+    cout << "Возраст студента: " << student.age << endl;
+    cout << "Год поступления: " << student.year << endl;
+    cout << "Оценки студента по математике, физике, аналитической геометрии и высшей математике соответственно: ";
+    cout << student.math << ", " << student.phys << ", " << student.analitic_geom << ", " << student.high_math << endl;
 
-class Mathematic: public Student {
-public:
-	double average_value(int math, int phys, int analitic_geom, int high_math) {
-		double av_value = (math + analitic_geom + phys + high_math) / 4;
-		return av_value;
-	}
-protected:
-	int math = 5;
-	int phys = 4;
-	int analitic_geom = 4;
-	int high_math = 4;
-	double check(int math, int phys, int analitic_geom, int high_math) {
-		if (average_value(math, phys, analitic_geom, high_math) < 3) cout << "Здравствуй небо в облаках, здравствуй юность в сапогах..." << endl;
-		else cout << "Ура, я умный!" << endl;
-	}
-};
+    // Вызов функций
+    int new_age = student.ch_age();
+    cout << "Новый возраст студента: " << new_age << endl;
 
-class Mathematic_prog : protected Mathematic {
-private:
-	int choose_mark(int math, int phys, int analitic_geom, int high_math) {
-		cout << "Введите оценки по предметам: "
-	}
-};
-
- int main() {
-	 system("chcp 1251>NULL");
-
- }
+}
